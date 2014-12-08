bb80fa3e9821115d2ea60fa24d8ec6a86f48aeff
Fatiatum <ei12030@fe.up.pt>
2014-12-08 14:16:12 +0000

    display working

--- src/StarBattle.pl
+++ src/StarBattle.pl
@@ -7,7 +7,11 @@
 :- use_module(library(clpfd)).
 :-use_module(library(random)).
 
-%starBattle:-
+starBattle:-init.
+
+init:-
+        makeBoard(B),
+        printBoard(B).
        
 boardSize(S):-
         write('What size do you want the board game to have? (it must be between 2 and 20)'), nl,
@@ -20,29 +24,34 @@ boardSize(S):-
            write('invalid size!'),
            boardSize(S)
         ).
-makeBoard(Nb):-
+%makeBoard(Nb):-
+%        boardSize(S),
+%        fillBoard(S, Rows, 0),
+%        fillBoard(S, B, Rows),
+%        makeDivs(S, B, New),
+%        Nb=New.
+
+makeBoard(B):-
         boardSize(S),
         fillBoard(S, Rows, 0),
-        fillBoard(S, B, Rows),
-        makeDivs(S, B, New),
-        Nb=New.
+        fillBoard(S, B, Rows).
 
-makeDivs(S, B, Nb):-
-        X #= (S * S),
-        div(S, B, 0, X, Nb).
-
-div(_,_,6,0,_).
-div(_S, B, 5, X, Nb):-
-        makeRandDiv(B, X, 5, Nb).
-div(S, B, C, X, Nb):-
-        Nc #= (C+1),
-        R = (X - (S-C)),
-        random(1,R+1,F),
-        makeRandDiv(B, F, Nc, Nb),
-        Nx = X-R,
-        div(S, B, Nc, Nx, Nb).
-
-makeRandDiv(B, F, Nc, Nb):-
+%makeDivs(S, B, Nb):-
+%        E #= (S * S),
+%        div(S, B, 0, E, 0, 0, Nb).
+%
+%div(_,_,6,0,_).
+%div(_S, B, 5, E, X, Y, Nb):-
+%        makeRandDiv(B, E, 5, X, Y, Nb).
+%div(S, B, C, E, X, Y, Nb):-
+%        Nc #= (C+1),
+%        R = (E - (S-C)),
+%        random(1,R+1,F),
+%        makeRandDiv(B, F, Nc, X, Y, Nb),
+%        Ne = E-R,
+%        div(S, B, Nc, Ne, Nb).
+%
+%makeRandDiv(B, F, Nc, Nb):-
         
                        
 
@@ -51,6 +60,85 @@ fillBoard(S, [V|Tail], V):-
         S>0,
         Ns is S-1,
         fillBoard(Ns, Tail, V).
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+printElement(0, ' e ').
+printELement(1, ' o ').
+
+printBoard(B):-
+        length(B,S),
+        write('        '),
+        F is S+2,
+        printBorder(0,F),nl,
+        printBoard(B,0,S),
+        write('        '),
+        printBorder(0,F).
+
+printBorder(0,S):-
+        Nh is 1,
+        write('X'),
+        printBorder(Nh,S).
+
+printBorder(H,S):-
+        H>S-2.
+
+printBorder(H,S):-
+        Nh is H+1,
+        write('XXXX'),
+        printBorder(Nh,S).
+                
+
+printBoard([],S,S).
+printBoard([X|Nb], H, S):-
+        Nh is H+1,
+        write('        '),
+        write('X'),
+        printLine(X, 0, S),
+        write('X'),nl,
+        write('        '),
+        write('X'),
+        printDiv(0,S),
+        write('X'),nl,
+        printBoard(Nb,Nh,S).
+
+printLine([],S,S).
+printLine([X|Nb],H,S):-
+        Ns is S-2,
+        H>Ns,
+        Nh is H+1,
+        printElement(X,Y),
+        write(Y),
+        printLine(Nb,Nh,S).
+
+printLine([X|Nb], H, S):-
+        Nh is H+1,
+        printElement(X,Y),
+        write(Y),
+        write('|'),
+        printLine(Nb,Nh,S). 
+
+printDiv(S,S).
+printDiv(0,S):-
+        Nh is 1,
+        write('---'),
+        printDiv(Nh,S).
+
+printDiv(H,S):-
+        Nh is H+1,
+        write('|---'),
+        printDiv(Nh,S).
+        
+        
+        
+        
+        
         
 
 
